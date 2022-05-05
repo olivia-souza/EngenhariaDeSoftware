ced7d63e2bad75d1d79b2a5f88989b88cc64754a
LiviaPires <livia.onofre@fatec.sp.gov.br>
2022-05-05 18:46:01 -0300

    Feat: Incremento ao código

--- Codigo_cadastro_quadras/Sistema_Quadras/src/Modulos/cadastro_quadras.java
+++ Codigo_cadastro_quadras/Sistema_Quadras/src/Modulos/cadastro_quadras.java
@@ -1,26 +1,47 @@
 package Modulos;
-import java.util.Scanner;
-import javax. swing. JOptionPane;
+import javax.swing.*;
+import java.util.Objects;
+//import java.util.*;
 
-public class cadastro_quadras {
-	public static String quadras = "";
-	public static void main (String [] args) {
-		int continuar_loop;
-				Scanner in = new Scanner(System.in); 
-		
-			do {
-				System.out.println("Qual quadra deseja cadastrar ? "); 
-				
-				quadras += in.nextLine() + " " ;
-							
-				continuar_loop = JOptionPane.showConfirmDialog(null,"Deseja cadastrar mais quadras ?","Confirme",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE, null);
-			
-			} while(continuar_loop == JOptionPane.YES_OPTION);
-		
-				in.close();
-		
-			System.out.println("As quadras cadastradas são: " + quadras);
-	}
+public class CadastroQuadras {
+    public static Object quadras;
+    public static void cadastrarQuadras(){
+        int continuar_loop;
+        do {
+            quadras = JOptionPane.showInputDialog("Digite qual quadra deseja cadastrar: ");
+            continuar_loop = JOptionPane.showConfirmDialog(null,"Deseja cadastrar mais quadras?","Confirme",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE, null);
+        }  while(continuar_loop == JOptionPane.YES_OPTION);
+    }
+    public static void quadrasDisponiveis(){
+        JOptionPane.showConfirmDialog(null, quadras,"As quadras cadastradas sao:", JOptionPane.OK_CANCEL_OPTION);
+    }
+    public static void reservarQuadras(){
+        int opcao = JOptionPane.showConfirmDialog(null, "Deseja reservar uma quadra?", "Reserva", JOptionPane.YES_NO_OPTION);
+        if (opcao == JOptionPane.YES_OPTION) {
+            quadrasDisponiveis();
+            int sim_nao = JOptionPane.showConfirmDialog(null, "Reservar Quadra?","Reserva",JOptionPane.YES_NO_OPTION);
+            if (sim_nao == JOptionPane.YES_NO_OPTION){
+                quadras = "Reservada: " + quadras;
+            }
+        }else{
+            inicio();
+        }
+    }
+    public static void inicio(){
+        do {
+            String valor = JOptionPane.showInputDialog(null, "Escolha o que fazer\na. Cadastrar Quadra\nb. Visualizar Quadras\nc. Reservar Quadra", "Escolha", JOptionPane.QUESTION_MESSAGE);
+            if (Objects.equals(valor, "a")) {
+                cadastrarQuadras();
+            } else if (Objects.equals(valor, "b")) {
+                quadrasDisponiveis();
+            } else if (Objects.equals(valor, "c")) {
+                reservarQuadras();
+            } else {
+                return;
+            }
+        }while (true);
+    }
+    public static void main(String[] args) {
+        inicio();
+    }
 }
-
-	
