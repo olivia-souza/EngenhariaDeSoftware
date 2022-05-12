ba978539deee7875ce7420a3a6c548cf645d57b2
LiviaPires <livia.onofre@fatec.sp.gov.br>
2022-05-12 16:25:09 -0300

    Fix: Modificando formato do projeto novamente

---
+++ QuadrasDeTenis/src/recursos/ConexaoBD.java
@@ -0,0 +1,24 @@
+package recursos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConexaoBD {
+	public static void main(String[] args) import java.sql.SQLException;{
+		//Conectar
+		String strConexaoMySQL = "jdbc:mysql://ip_da_maquina_do_bd:5432/nomedobanco";
+		Connection conexao = DriverManager.getConnection(strConexaoMySQL, "usuario", "senha");
+		//Executar
+		PreparedStatement sql = conexao.prepareStatement("SELECT numero, tenis, coberta FROM quadras");
+		ResultSet resultado = sql.executeQuery();
+		while(resultado.next()) {
+			System.out.printf("%d : %s (%s)", resultado.getInt("numero"), resultado.getBoolean("tenis") ? "Quadra de tênis" : "Quadra de beach tênis", resultado.getBoolean("coberta") ? "coberta" : "descoberta");
+		}
+		//Fechar conexão
+		System.out.println("Conexão Criada");
+		conexao.close();
+	}
+
+}
