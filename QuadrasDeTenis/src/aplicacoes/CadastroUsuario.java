72cc03df348bf6ebd7c2a2fc9953997a397a74c5
Micazuke <yvmalinowski@gmail.com>
2022-05-26 06:27:26 -0300

    Feat: incremento cadastro usuario

--- QuadrasDeTenis/src/aplicacoes/CadastroUsuario.java
+++ QuadrasDeTenis/src/aplicacoes/CadastroUsuario.java
@@ -1,6 +1,29 @@
 package aplicacoes;
 
-public class CadastroUsuario {
+import java.util.Scanner;
 
+public class CadastroUsuario {
+	
+		private static String Nome, cpf, email, senha;
 		
+		public static void main (String [] args ) {
+				Scanner receber = new Scanner (System.in);
+				
+				do{
+				System.out.println("Digite seu nome: ");
+				Nome = receber.nextLine();
+				}while (Nome.length() < 4 || Nome.length() > 50 );	
+				
+				do{
+				System.out.println("Digite seu cpf: ");
+				cpf = receber.nextLine();
+				}while (cpf.length() != 11);
+				
+				System.out.println("Digite seu email: ");
+				email = receber.nextLine();
+				
+				System.out.println("Digite sua senha: ");
+				senha = receber.nextLine();
+				
+		}
 }
