195bfff110e054cc51b991375546caad3f942dcb
Micazuke <yvmalinowski@gmail.com>
2022-05-27 15:18:43 -0300

    remocao arquivo txt

---
+++ QuadrasDeTenis/src/aplicacoes/CalculoCusto.java
@@ -0,0 +1,11 @@
+package aplicacoes;
+import java.util.Scanner;
+public class CalculoCusto extends Reserva {
+
+        public void calcularcusto { 
+            Scanner receber = new Scanner (System.in);
+            
+        }
+
+        
+}
